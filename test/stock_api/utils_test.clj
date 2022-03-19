0d0aaf25a340a7af902e0fd9de420fc688c8ba7f
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-03-19 14:29:16 -0300

    add unit tests to utils

---
+++ test/stock_api/utils_test.clj
@@ -0,0 +1,23 @@
+(ns stock-api.utils-test
+  (:require [midje.sweet :refer :all]
+            [stock-api.utils :refer [json-content]]))
+
+(facts "Convert content to JSON"
+       (let [response (json-content {:message "Hello World"})]
+         (fact "Status should be 200"
+               (:status response) => 200)
+         (fact "Headers should be 'application/json; charset=utf-8'"
+               (:headers response) => {"Content-Type" "application/json; charset=utf-8"})
+         (fact "Content should be 'Hello World'"
+               (:body response) => "{\"message\":\"Hello World\"}")))
+
+(facts "Convert content to JSON with status 201"
+       (let [response (json-content {:message "Hello Clojure!"} 201)]
+         (fact "Status should be 201"
+               (:status response) => 201)
+         (fact "Headers should be 'application/json; charset=utf-8'"
+               (:headers response) => {"Content-Type" "application/json; charset=utf-8"})
+         (fact "Content should be 'Hello Clojure!'"
+               (:body response) => "{\"message\":\"Hello Clojure!\"}")))
+
+
